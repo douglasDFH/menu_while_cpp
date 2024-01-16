deb529c90251aca775015a021b6c7882231719c4
douglasDFH <155786486+douglasDFH@users.noreply.github.com>
2024-01-16 17:14:43 -0400

    Add files via upload
    ejercicio elaborado con la funcion while

---
+++ while_menu.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int opcion;
+	opcion=1;
+	while(opcion !=0){
+		cout<<"menu principal"<<endl;
+		cout<<"1.- opcion uno "<<endl;
+		cout<<"2.-opcion dos"<<endl;
+		cout<<"0.-salir "<<endl;
+		cin>>opcion;
+		switch(opcion){
+		case 1:cout<<"eligio la opcion 1"<<endl;
+		break;
+		case 2:cout<<"eligio la opcion 2"<<endl;
+		break;
+		case 0:cout<<"eloigio la opcion salir"<<endl;
+		break;
+		default:cout<<"no escojio una opcion correcta"<<endl;
+		
+		};
+	};
+	return 0;
+}
+
